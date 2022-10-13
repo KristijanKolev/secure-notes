d06d19820adf36236d20faa6542779af28ddfe65
KristijanKolev <kikokolev@gmail.com>
2022-10-13 09:51:50 +0200

    Main model entities added.

--- secure-notes-rest/encrypted_notes/models.py
+++ secure-notes-rest/encrypted_notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class EncryptedNote(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.BinaryField(editable=True, blank=True)
+
+
+class NoteAccessKey(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    note = models.ForeignKey(EncryptedNote, related_name='access_keys', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    salt = models.BinaryField(max_length=16)
+    encrypted_key = models.BinaryField()
